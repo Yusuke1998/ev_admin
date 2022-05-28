cd5844266e6f097ef97620b25ec296c5edb673e7
yusuke1998 <jhonnyjose1998@gmail.com>
2022-05-28 15:48:20 -0400

    [UPD] ajustes en api

--- controllers/mobile_controller.py
+++ controllers/mobile_controller.py
@@ -25,18 +25,18 @@ class MobileController(http.Controller):
         })
 
         if believer:
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully registered',
                 'record': {
                     'id': believer.id
                 }
-            })
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error registering'
-            })
+            }
 
     @http.route('/update/believer/<int:believer_id>', type='json', auth="user", methods=['POST'])
     def UpdateBeliever(self, believer_id, **post):
@@ -47,8 +47,8 @@ class MobileController(http.Controller):
 
         if believer:
             believer.write({
-                'name': post.get('name'),
                 'profile': True,
+                'name': post.get('name'),
                 'identity': post.get('identity'),
                 'state_id': post.get('state_id'),
                 'municipality_id': post.get('municipality_id'),
@@ -69,18 +69,18 @@ class MobileController(http.Controller):
                         'password': post.get('password')
                     })
 
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully updated',
                 'record': {
                     'id': believer.id
                 }
-            })
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error updating'
-            })
+            }
 
     @http.route('/believers', type='json', auth="user", methods=['GET'])
     def GetBelievers(self, **post):
@@ -88,51 +88,87 @@ class MobileController(http.Controller):
         believers = model_believer.search([('active', '=', True)])
 
         if believers:
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully retrieved',
-                'records': [
-                    {
+                'records': [{
                         'id': believer.id,
-                        'name': believer.name
-                    } for believer in believers
-                ]
-            })
+                        'name': believer.name,
+                        'identity': believer.identity,
+                        'state': {
+                            'id': believer.state_id.id,
+                            'name': believer.state_id.name
+                        },
+                        'municipality': {
+                            'id': believer.municipality_id.id,
+                            'name': believer.municipality_id.name
+                        },
+                        'parish': {
+                            'id': believer.parish_id.id,
+                            'name': believer.parish_id.name
+                        },
+                        'sector': believer.sector,
+                        'street': believer.street,
+                        'building': believer.building,
+                        'house': believer.house,
+                        'localphone_number': believer.localphone_number,
+                        'localphone_number': believer.cellphone_number,
+                        'department_ids': [{
+                            'id': department.id,
+                            'name': department.name
+                        } for department in believer.department_ids],
+                        'profile': believer.profile
+                } for believer in believers ]
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error retrieving'
-            })
+            }
 
-    @http.route('/believers/<int:believer_id>', type='json', auth="user", methods=['GET'])
-    def GetBeliever(self, believer_id, **post):
+    @http.route('/believer/<int:believer_id>', type='json', auth="user", methods=['GET'])
+    def GetBeliever(self, believer_id):
         model_believer = request.env['ev.believer']
         believer = model_believer.search([('id', '=', believer_id)])
 
         if believer:
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully retrieved',
                 'record': {
                     'id': believer.id,
                     'name': believer.name,
                     'identity': believer.identity,
-                    'state_id': believer.state_id.id,
-                    'municipality_id': believer.municipality_id.id,
-                    'parish_id': believer.parish_id.id,
+                    'state': {
+                        'id': believer.state_id.id,
+                        'name': believer.state_id.name
+                    },
+                    'municipality': {
+                        'id': believer.municipality_id.id,
+                        'name': believer.municipality_id.name
+                    },
+                    'parish': {
+                        'id': believer.parish_id.id,
+                        'name': believer.parish_id.name
+                    },
                     'sector': believer.sector,
                     'street': believer.street,
                     'building': believer.building,
                     'house': believer.house,
                     'localphone_number': believer.localphone_number,
-                    'cellphone_number': believer.cellphone_number
+                    'localphone_number': believer.cellphone_number,
+                    'department_ids': [{
+                        'id': department.id,
+                        'name': department.name
+                    } for department in believer.department_ids],
+                    'profile': believer.profile                    
                 }
-            })
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error retrieving'
-            })
+            }
 
     @http.route('/departments', type='json', auth="user", methods=['GET'])
     def GetDepartments(self, **post):
@@ -140,7 +176,7 @@ class MobileController(http.Controller):
         departments = model_department.search([('active', '=', True)])
 
         if departments:
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully retrieved',
                 'records': [
@@ -149,34 +185,37 @@ class MobileController(http.Controller):
                         'name': department.name
                     } for department in departments
                 ]
-            })
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error retrieving'
-            })
+            }
 
     @http.route('/news', type='json', auth="user", methods=['GET'])
-    def GetNews(self, **post):
+    def GetNews(self):
         model_news = request.env['ev.news']
         news = model_news.search([
             ('published', '=', True)
         ], order='date desc')
 
         if news:
-            return json.dumps({
+            return {
                 'status': 'success',
                 'message': 'Successfully retrieved',
-                'records': [
-                    {
-                        'id': new.id,
-                        'title': new.title,
-                        'description': new.description,
-                    } for new in news
-                ]
-            })
+                'records': [{
+                    'id': new.id,
+                    'title': new.title,
+                    'description': new.description,
+                    'date': new.date,
+                    'expiry_date': new.expiry_date,
+                    'image': new.image,
+                    'image_url': new.image_url,
+                    'state': new.state
+                } for new in news]
+            }
         else:
-            return json.dumps({
+            return {
                 'status': 'error',
                 'message': 'Error retrieving'
-            })
+            }
